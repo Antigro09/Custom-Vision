82a49aad1c5720c5862072b7c735cf66ae7f4242
Anthony Cavero <anthonyj.cavero@gmail.com>
2026-03-31 22:07:55 -0400

    Refactor Publisher class and remove unused includes

--- src/publisher.hpp
+++ src/publisher.hpp
@@ -1,69 +1,22 @@
 #pragma once
-
-#include "pose.hpp"
-
-#include <apriltag.h>
-#if HAVE_NTCORE
-#include <networktables/BooleanTopic.h>
-#include <networktables/DoubleArrayTopic.h>
-#include <networktables/DoubleTopic.h>
-#include <networktables/IntegerArrayTopic.h>
-#include <networktables/IntegerTopic.h>
-#include <networktables/NetworkTableInstance.h>
-#endif
-
-#include <cstdint>
-#include <optional>
 #include <string>
-#include <utility>
 #include <vector>
+#include "types.hpp"
 
-struct PublisherOptions {
-    std::string camera_name = "camera0";
-    std::optional<unsigned int> team_number;
-    std::optional<std::string> server;
-    bool use_stdout_json = false;
-};
+#if HAVE_NTCORE
+#include <networktables/NetworkTableInstance.h>
+#include <networktables/NetworkTable.h>
+#endif
 
 class Publisher {
 public:
-    explicit Publisher(PublisherOptions options = {});
-    ~Publisher();
-
-    void publish(
-        bool connected,
-        std::int64_t frame_id,
-        std::int64_t timestamp_us,
-        double latency_ms,
-        double fps,
-        const std::vector<std::pair<apriltag_detection_t*, PoseResult>>& detections);
+  Publisher(int team, const std::string& tableName);
+  void publish(int64_t timestampUs, int frameId, const std::vector<DetectionResult>& detections);
 
 private:
-    void emitStdoutJson(
-        bool connected,
-        std::int64_t frame_id,
-        std::int64_t timestamp_us,
-        double latency_ms,
-        double fps,
-        const std::vector<std::pair<apriltag_detection_t*, PoseResult>>& detections) const;
-
-    std::string base_topic_;
-    bool use_stdout_json_;
 #if HAVE_NTCORE
-    nt::NetworkTableInstance nt_instance_;
-    nt::BooleanPublisher connected_pub_;
-    nt::IntegerPublisher frame_id_pub_;
-    nt::IntegerPublisher timestamp_us_pub_;
-    nt::DoublePublisher latency_ms_pub_;
-    nt::DoublePublisher fps_pub_;
-    nt::IntegerPublisher tag_count_pub_;
-    nt::IntegerArrayPublisher tag_ids_pub_;
-    nt::DoubleArrayPublisher decision_margins_pub_;
-    nt::DoubleArrayPublisher centers_xy_pub_;
-    nt::DoubleArrayPublisher corners_xy_pub_;
-    nt::DoubleArrayPublisher translations_m_pub_;
-    nt::DoubleArrayPublisher rotations_rvec_rad_pub_;
-    nt::DoubleArrayPublisher euler_deg_pub_;
-    nt::DoubleArrayPublisher distances_m_pub_;
+  nt::NetworkTableInstance inst_;
+  std::shared_ptr<nt::NetworkTable> table_;
 #endif
+  bool enabled_{false};
 };
