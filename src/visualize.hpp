18145200285609951918a1bb2f4ade5c1a0e6853
Anthony Cavero <anthonyj.cavero@gmail.com>
2026-03-31 22:10:51 -0400

    Refactor detection drawing functions in visualize.hpp

--- src/visualize.hpp
+++ src/visualize.hpp
@@ -1,15 +1,6 @@
 #pragma once
-
-#include "pose.hpp"
-
-#include <apriltag.h>
 #include <opencv2/core.hpp>
+#include <vector>
+#include "types.hpp"
 
-void drawDetection(
-    cv::Mat& frame,
-    const apriltag_detection_t* detection,
-    const PoseResult& pose,
-    const cv::Mat& camera_matrix,
-    const cv::Mat& dist_coeffs);
-
-void drawFps(cv::Mat& frame, double fps);
+void drawDetections(cv::Mat& frame, const std::vector<DetectionResult>& detections, double fps);
