766258af56a0d645d0a11552f2b1c21727caba0b
Anthony Cavero <anthonyj.cavero@gmail.com>
2026-03-31 22:06:53 -0400

    Refactor pose estimation function and structure

--- src/pose.hpp
+++ src/pose.hpp
@@ -1,20 +1,14 @@
 #pragma once
+#include <optional>
+#include "types.hpp"
+#include "calibration.hpp"
 
+extern "C" {
 #include <apriltag.h>
-#include <opencv2/core.hpp>
+}
 
-struct PoseResult {
-    cv::Vec3d tvec;
-    cv::Vec3d rvec;
-    cv::Mat rotation_matrix;
-    double roll_deg;
-    double pitch_deg;
-    double yaw_deg;
-    double distance_m;
-};
-
-PoseResult estimatePose(
-    const apriltag_detection_t* detection,
-    const cv::Mat& camera_matrix,
-    const cv::Mat& dist_coeffs,
-    double tag_size_meters);
+std::optional<DetectionResult> estimatePoseFromDetection(
+  const apriltag_detection_t* det,
+  const Calibration& calib,
+  double tagSizeM
+);
